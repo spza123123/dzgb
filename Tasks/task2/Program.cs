3f546aac03a669b92c7fa64d8cc8a40063782abb
Zakhar <info@zspirin.ru>
2024-02-09 15:14:43 +0300

    Второе задание

---
+++ Tasks/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Введите x: ");
+int x;
+int y;
+x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите y: ");
+y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("1");
+}
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("3");
+}
+else if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("4");
+}
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("2");
+}
