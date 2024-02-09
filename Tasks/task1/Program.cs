4f2caed513c11a779ac354ea34c0375e810c1d35
Zakhar <info@zspirin.ru>
2024-02-09 15:12:33 +0300

    Первое задание

---
+++ Tasks/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число: ");
+int num;
+num = Convert.ToInt32(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0)
+{
+    System.Console.WriteLine("да"); //ПРИМЕР На  положительный вывод число 161
+}
+else
+{
+    System.Console.WriteLine("нет"); //ПРИВЕР На отрицательный вывод число 200
+}
+
